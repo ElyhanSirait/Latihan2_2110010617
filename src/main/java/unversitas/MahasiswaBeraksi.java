a1d2a0137b9fbdad41141e12dc62b15efcd00de6
ElyhanSirait <136247696+ElyhanSirait@users.noreply.github.com>
2023-06-13 22:59:29 +0800

    MahasiswaBeraksi.java

--- src/main/java/unversitas/MahasiswaBeraksi.java
+++ src/main/java/unversitas/MahasiswaBeraksi.java
@@ -1,6 +1,12 @@
-
-package unversitas;
+package Universitas;
 
 public class MahasiswaBeraksi {
     
+    public static void main(String[] args) {
+        
+        mahasiswa  mahasiswa= new mahasiswa("Elyhan Sirait","2110010617","Teknik Informatika");
+        
+        System.out.println("Detail Mahasiswa");
+        System.out.println(mahasiswa.getDetail());
+    }
 }
